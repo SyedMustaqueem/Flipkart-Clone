ab00b89f82092b75e42642802ae0c935d89f4235
SyedMustaqueem <sd.mustaqueem66@gmail.com>
2024-02-07 17:11:22 +0530

    Add Clean Up Non Varified Users

---
+++ src/main/java/com/xmp/fkt/util/ScheadulerJobs.java
@@ -0,0 +1,25 @@
+package com.xmp.fkt.util;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.xmp.fkt.repository.UserRepo;
+import com.xmp.fkt.service.AuthService;
+
+import lombok.AllArgsConstructor;
+
+
+@Component
+@AllArgsConstructor
+public class ScheadulerJobs {
+	
+	UserRepo userRepo;
+private AuthService authService;
+
+@Scheduled(cron = "0 0 0 * * *") // Daily at midnight
+public void deleteNonVerifiedUsers()
+{
+   
+     userRepo.deleteByEmailVerifiedFalse();;
+}
+}
